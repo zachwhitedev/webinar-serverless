02227ab300b5bb95cc586212fd588feef6605abf
Zach White <zechnwhite@gmail.com>
2020-01-12 18:31:02 -0800

    adds create and list lambdas

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 - <code>cd</code> into the name of your API folder, in this case <code>webinarServerless</code>. Then...
-- created from template using command <code>serverless create --template aws-nodejs</code>
+- create your service from template using command <code>serverless create --template aws-nodejs</code>
 - <code>npm init</code> (this will allow you to use npm modules. Entry point should be set to <code>handler.js</code>)
 - <code>npm install aws-sdk --save</code>
 - <code>npm i aws-sdk --save</code>
--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 'use strict';
+const AWS = require('aws-sdk');
 
 module.exports = {
   create: async (event, context) => {
@@ -18,9 +19,66 @@ module.exports = {
       return {
         statusCode: 400
       }
-    } 
+    }
+
+    let putParams = {
+      TableName: process.env.DYNAMO_KITTEN_TABLE,
+      Item: {
+        name: bodyObj.name,
+        age: bodyObj.age
+      }
+    }
+    let putResult = {}
+    try {
+      let dynamodb = new AWS.DynamoDB.DocumentClient()
+      putResult = await dynamodb.put(putParams).promise()
+    } catch(putError) {
+      console.log('There was a problem putting the kitten.')
+      console.log('putParams', putParams);
+      return {
+        statusCode: 500
+      }
+    }
+
+    return {
+      statusCode: 201
+    }
+
   },
-  list: async (event, context) = {
+  list: async (event, context) => {
+    let scanParams = {
+      TableName: process.env.DYNAMO_KITTEN_TABLE
+    }
+    let scanResult = {}
+    try {
+      let dynamodb = new AWS.DynamoDB.DocumentClient()
+      scanResult = await dynamodb.scan(scanParams).promise()
+    } catch (scanError) {
+      console.log('There was a problem scanning the kittens.')
+      console.log('scanError', scanError)
+      return {
+        statusCode: 500
+      }
+    }
+
+    if(scanResult.Items == null || 
+      !Array.isArray(scanResult) || 
+      scanResult.Items.length == 0) {
+        return {
+          statusCode: 404,
+          body: JSON.stringify(scanResult.Items.map(kitten => {
+            return {
+              name: kitten.name,
+              age: kitten.age
+            }
+          }))
+        }
+    }
+
+    return {
+      statusCode: 200,
+
+    }
 
   },
   get: async (event, context) = {
