45fb4d9953f1d0ebb781064e91569aed87fc3ce9
Zach White <zechnwhite@gmail.com>
2020-01-12 18:59:28 -0800

    get function 2

--- handler.js
+++ handler.js
@@ -3,22 +3,24 @@ const AWS = require('aws-sdk');
 
 module.exports = {
   create: async (event, context) => {
-    let bodyObj = {}
+    let bodyObj = {};
     try {
-      bodyObj = JSON.parse(event.body)
-    } catch(jsonError) {
-      console.log('There was an error parsing the body', jsonError)
+      bodyObj = JSON.parse(event.body);
+    } catch (jsonError) {
+      console.log('There was an error parsing the body', jsonError);
       // console.log, when it comes to Lambda functions, gets printed to your CloudWatch logs
       return {
         statusCode: 400
-      }
+      };
     }
-    if(typeof bodyObj.name == 'undefined' || 
-    typeof bodyObj.age == 'undefined') {
-      console.log('Missing parameters')
+    if (
+      typeof bodyObj.name == 'undefined' ||
+      typeof bodyObj.age == 'undefined'
+    ) {
+      console.log('Missing parameters');
       return {
         statusCode: 400
-      }
+      };
     }
 
     let putParams = {
@@ -27,67 +29,80 @@ module.exports = {
         name: bodyObj.name,
         age: bodyObj.age
       }
-    }
-    let putResult = {}
+    };
+    let putResult = {};
     try {
-      let dynamodb = new AWS.DynamoDB.DocumentClient()
-      putResult = await dynamodb.put(putParams).promise()
-    } catch(putError) {
-      console.log('There was a problem putting the kitten.')
+      let dynamodb = new AWS.DynamoDB.DocumentClient();
+      putResult = await dynamodb.put(putParams).promise();
+    } catch (putError) {
+      console.log('There was a problem putting the kitten.');
       console.log('putParams', putParams);
       return {
         statusCode: 500
-      }
+      };
     }
 
     return {
       statusCode: 201
-    }
-
+    };
   },
   list: async (event, context) => {
     let scanParams = {
       TableName: process.env.DYNAMO_KITTEN_TABLE
-    }
-    let scanResult = {}
+    };
+    let scanResult = {};
     try {
-      let dynamodb = new AWS.DynamoDB.DocumentClient()
-      scanResult = await dynamodb.scan(scanParams).promise()
+      let dynamodb = new AWS.DynamoDB.DocumentClient();
+      scanResult = await dynamodb.scan(scanParams).promise();
     } catch (scanError) {
-      console.log('There was a problem scanning the kittens.')
-      console.log('scanError', scanError)
+      console.log('There was a problem scanning the kittens.');
+      console.log('scanError', scanError);
       return {
         statusCode: 500
-      }
+      };
     }
 
-    if(scanResult.Items == null || 
-      !Array.isArray(scanResult) || 
-      scanResult.Items.length == 0) {
-        return {
-          statusCode: 404,
-          body: JSON.stringify(scanResult.Items.map(kitten => {
-            return {
-              name: kitten.name,
-              age: kitten.age
-            }
-          }))
-        }
+    if (
+      scanResult.Items == null ||
+      !Array.isArray(scanResult) ||
+      scanResult.Items.length == 0
+    ) {
+      return {
+        statusCode: 404
+      };
     }
 
     return {
       statusCode: 200,
-
+      body: JSON.stringify(
+        scanResult.Items.map(kitten => {
+          return {
+            name: kitten.name,
+            age: kitten.age
+          };
+        })
+      )
+    };
+  },
+  get: async (event, context) => {
+    let getParams = {
+      TableName: process.env.DYNAMO_KITTEN_TABLE,
+      Key: {
+        name: event.pathParameters.name
+      }
     }
-
+    try{
+      let dynamodb = new AWS.DynamoDB.DocumentClient()
+      getResult = dynamodb.get(getParams).promise()
+    } catch(getError){
+      console.log('There was a problem scanning the kittens')
+      console.log('getError', getError)
+      return {
+        statusCode: 500
+      }
+    }
   },
-  get: async (event, context) = {
 
-  },
-  update: async (event, context) = {
-
-  },
-  delete: async (event, context) = {
-
-  },
+  update: async (event, context) => {},
+  delete: async (event, context) => {}
 };
